5fc536edd3ab328f6d61661eb2342db256cdb90c
moiz <moiz@samtech-solutions.com>
2025-09-25 10:26:24 +0000

    test

---
+++ hebatullah_customizations/hebatullah_customizations/report/sales_register___simple/sales_register___simple.js
@@ -0,0 +1,89 @@
+// Copyright (c) 2025, Samtech and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Register - Simple"] = {
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			width: "80",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
+		},
+		{
+			fieldname: "customer_group",
+			label: __("Customer Group"),
+			fieldtype: "Link",
+			options: "Customer Group",
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+		},
+		{
+			fieldname: "mode_of_payment",
+			label: __("Mode of Payment"),
+			fieldtype: "Link",
+			options: "Mode of Payment",
+		},
+		{
+			fieldname: "owner",
+			label: __("Owner"),
+			fieldtype: "Link",
+			options: "User",
+		},
+		{
+			fieldname: "cost_center",
+			label: __("Cost Center"),
+			fieldtype: "Link",
+			options: "Cost Center",
+		},
+		{
+			fieldname: "warehouse",
+			label: __("Warehouse"),
+			fieldtype: "Link",
+			options: "Warehouse",
+		},
+		{
+			fieldname: "brand",
+			label: __("Brand"),
+			fieldtype: "Link",
+			options: "Brand",
+		},
+		{
+			fieldname: "item_group",
+			label: __("Item Group"),
+			fieldtype: "Link",
+			options: "Item Group",
+		},
+		{
+			fieldname: "include_payments",
+			label: __("Show Ledger View"),
+			fieldtype: "Check",
+			default: 0,
+		},
+		{
+			fieldname: "show_returns",
+			label: __("Show Returns"),
+			fieldtype: "Check",
+			default: 0,
+		},
+	],
+};
+
+erpnext.utils.add_dimensions("Sales Register - Simple", 7);
